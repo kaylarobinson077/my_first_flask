6021737e85f7ddd2a88b68ba7db3abda92be0853
krobinso <krobinso@amfam.com>
2020-11-09 08:29:06 -0600

    start incorping db

--- personal_site/app/__init__.py
+++ personal_site/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config) # config -> module, Config -> class
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes # at bottom, needs to import app var defined above
+from app import routes, models # at bottom, needs to import app var defined above
--- personal_site/personal_site.py
+++ personal_site/personal_site.py
@@ -3,3 +3,9 @@ from app import app # flask app is member of app package
 # to run, in venv type
 # export FLASK_APP=personal_site.py
 # flask run
+
+# to manage migrating databases,
+# flask db init    --> create migration repository
+# flask db migrate --> generate automatic migrations
+# flask db upgrade --> apply migration scripts to the database
+# flask db downgrade --> undo the recent migration upgrade
